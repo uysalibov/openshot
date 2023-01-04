b7cb910e17710cf92582e677fe1bae3fabf87c1a
İbrahim Uysal <uysalibov@gmail.com>
2023-01-04 17:49:25 +0300

    unnecessary auth functions deleted

--- auth.py
+++ auth.py
@@ -6,6 +6,7 @@ from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 from models import User, UserInDB, TokenData
+from database import sel_userByUsername
 
 SECRET_KEY = "ede21b654b636786aef0f2ad40d353bf9fd70a54d97252b7d83536bc774de923"
 ALGORITHM = "HS256"
@@ -27,22 +28,14 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
-
 def get_password_hash(password):
     return pwd_context.hash(password)
 
-
-def get_user(db, username: str):
-    if username in db:
-        user_dict = db[username]
-        return UserInDB(**user_dict)
-
-
-def authenticate_user(fake_db, username: str, password: str):
-    user = get_user(fake_db, username)
+def authenticate_user(username: str, password: str):
+    user = sel_userByUsername(username)
     if not user:
         return False
-    if not verify_password(password, user.hashed_password):
+    if not verify_password(password, user.hashedPass):
         return False
     return user
 
@@ -72,13 +65,8 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
         token_data = TokenData(username=username)
     except JWTError:
         raise credentials_exception
-    user = get_user(fake_users_db, username=token_data.username)
+    user = sel_userByUsername(token_data.username)
+    print(user)
     if user is None:
         raise credentials_exception
-    return user
-
-
-async def get_current_active_user(current_user: User = Depends(get_current_user)):
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
-    return current_user
+    return dict(user)
