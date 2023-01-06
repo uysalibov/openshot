fe31348875783a780bc26fe05a6e56a05e3f2686
İbrahim Uysal <uysalibov@gmail.com>
2023-01-06 23:01:11 +0300

    Added project model for new project endpoint

--- models.py
+++ models.py
@@ -1,7 +1,6 @@
 from pydantic import BaseModel
 from typing import Union
 
-
 class Token(BaseModel):
     access_token: str
     token_type: str
@@ -12,6 +11,15 @@ class TokenData(BaseModel):
 class User(BaseModel):
     id: int
     username: str
+    
+class Project(BaseModel):
+    name: str
+    width: int
+    height: int
+    fps: int = 30
+    sample_rate: int = 44100
+    channels: int = 2 # Audio channels (2 = stereo)
+    channels_layout: int = 3  # Channel layout type (3=stereo, 4=mono, 7=Surround)
 
 class UserInDB(User):
     hashed_password: str
