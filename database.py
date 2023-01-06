2a641faa3ac40c574d70feaa51eb3567ee7e2730
İbrahim Uysal <uysalibov@gmail.com>
2023-01-06 23:02:12 +0300

    Added new db method for insert new project

--- database.py
+++ database.py
@@ -1,8 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from database_model import Users
-
 from typing import Union
+import uuid
+
+from database_model import Projects, User
+from models import Project, User as MUser
 
 engine = create_engine("sqlite:///openshot.db")
 Session = sessionmaker(bind=engine)
@@ -12,5 +14,22 @@ def sel_userByUsername(username:str):
         Get users filtered by username
     """
     session = Session()
-    q = session.query(Users).filter(Users.c.username==username)
-    return dict(q[0]) if q.count() > 0 else None
+    q = session.query(User).filter(User.username==username)
+    return q[0].__dict__ if q.count() > 0 else None
+
+def ins_newProject(request_model: Project, owner: MUser):
+    
+    session = Session()
+    model = request_model.dict()
+    name = model.pop("name") # we remove it because our request model and database model have different name
+    
+    new_project = Projects(
+        projectId=str(uuid.uuid4().hex),
+        ownerId=owner["id"],
+        projectName=name,
+        **model
+    )
+    session.add(new_project)
+    session.commit()
+    return new_project.projectId
+    
