b985e359056afb6394f7efb8f3e9070758e20a8a
İbrahim Uysal <uysalibov@gmail.com>
2023-01-06 23:00:05 +0300

    Added endpoint for create new project

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ import uvicorn
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 
-from models import Token, User
+from models import Token, User, Project
 from auth import (
     authenticate_user,
     ACCESS_TOKEN_EXPIRE_MINUTES,
     create_access_token,
     get_current_user
 )
-
+from database import ins_newProject
 
 app = FastAPI()
 
@@ -26,7 +26,7 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
         )
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires
+        data={"sub": user["username"]}, expires_delta=access_token_expires
     )
     return {"access_token": access_token, "token_type": "bearer"}
 
@@ -34,6 +34,12 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
 @app.get("/users/me/", response_model=User, tags=["Authentication"])
 async def read_users_me(current_user: User = Depends(get_current_user)):
     return current_user
+
+@app.post("/projects/new", tags=["projects"])
+async def new_project(current_user: User = Depends(get_current_user), project_details: Project = Depends()):
+    r = ins_newProject(project_details, current_user)
+    return {"projectId": f"{r}"}
+    
 
         
 if __name__ == "__main__":
